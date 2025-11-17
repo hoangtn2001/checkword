1f62d04b3298f5df6f0e62d1a876a468ac5de0da
hoangtn2001 <bachnhan11a9@gmail.com>
2025-11-17 17:55:23 +0700

    check err

--- src/components/Square/Square.tsx
+++ src/components/Square/Square.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import './square.css';
 import { motion } from 'framer-motion';
 import { useSelector } from 'react-redux';
